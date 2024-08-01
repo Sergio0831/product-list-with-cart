f0fd2786d092ba0806a7712781a66b181314d2d7
Sergio0831 <sergejs.ivcenko@gmail.com>
2024-08-01 18:47:31 +0100

    Resolve Typescript type inference with lazy loading components

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { Toaster } from 'react-hot-toast';
 import products from './data/products.json';
 import { lazy, Suspense } from 'react';
 import { Wrapper } from './components';
+import { ProductTypes } from './types';
+import { ListProps } from './components/List';
 
 const Cart = lazy(() => import('./components/Cart'));
-const List = lazy(() => import('./components/List'));
+const List = lazy(() => import('./components/List')) as React.FC<ListProps<ProductTypes>>;
 const Product = lazy(() => import('./components/Product'));
 
 function App() {
@@ -28,8 +30,8 @@ function App() {
           <section className="col-span-full sm:col-span-8 md:col-span-7 lg:col-span-8">
             <h1 className="text-preset-1 mb-8">Desserts</h1>
             <List
-              items={products}
-              renderItem={(product) => <Product product={product} />}
+              items={products as ProductTypes[]}
+              renderItem={(product: ProductTypes) => <Product product={product} />}
               className="sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6"
             />
           </section>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,33 @@
 import Button from './Button';
 import Cart from './Cart';
 import CartEmpty from './CartEmpty';
+import CartProduct from './CartProduct';
+import CartTotal from './CartTotal';
+import { Drawer, DrawerContent, DrawerTrigger } from './Drawer';
 import List from './List';
+import { Modal, ModalContent, ModalTrigger } from './Modal';
+import ModalOrDrawer from './ModalOrDrawer';
+import ModalProduct from './ModalProduct';
 import Product from './Product';
+import QuantityButton from './QuantityButton';
 import Wrapper from './Wrapper';
 
-export { Button, Cart, CartEmpty, List, Product, Wrapper };
+export {
+  Button,
+  Cart,
+  CartEmpty,
+  List,
+  Product,
+  Wrapper,
+  CartTotal,
+  Drawer,
+  DrawerContent,
+  DrawerTrigger,
+  Modal,
+  ModalContent,
+  ModalTrigger,
+  ModalOrDrawer,
+  ModalProduct,
+  QuantityButton,
+  CartProduct,
+};
